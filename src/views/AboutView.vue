d005a9fb6b4fe6c2989087c3393970a6840bde05
Andy Lin <ld130102@gmail.com>
2023-05-03 17:23:55 +0800

    v-if&v-show

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="about">
-    <h1 v-if="shown">This is an about page</h1>
-    <button @click="clickHandler">{{ shown === true ? 'HIDE' : 'SHOW'  }}</button>
+    <h1>v-if</h1>
+    <h2 v-if="shown">Hello A</h2>
+    <h2 v-else>Hello B</h2>
+    <button @click="clickHandler">{{ shown === true ? 'Show B' : 'Show A'  }}</button>
+    <h1>v-show</h1>
+    <h2 v-show="shown2">Hello A</h2>
+    <h2 v-show="!shown2">Hello B</h2>
+    <button @click="clickHandler2">{{ shown2 === true ? 'Show B' : 'Show A'  }}</button>
   </div>
 </template>
 
@@ -11,9 +17,13 @@ import { ref } from 'vue';
 
 
 const shown = ref(true);
+const shown2 = ref(true);
 
 const clickHandler = ()=> {
   shown.value = !shown.value;
+}
+const clickHandler2 = ()=> {
+  shown2.value = !shown2.value;
 }
 
 </script>
@@ -22,5 +32,8 @@ const clickHandler = ()=> {
 <style>
 .about {
   width: 300px;
+}
+h2 {
+  color: red
 }
 </style>
